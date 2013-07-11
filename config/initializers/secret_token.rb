1782b5feff38038af31f3cb8322ab9dbb6de240a
Benjamin Michael Brown <benjaminbb@gmail.com>
2013-07-11 14:03:00 -0500

    initial commit for bwitter (Ben's Twitter)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bwitter::Application.config.secret_token = 'c651e40b8e8ad5a9febfe8e12cabe1ed7816b3be6f1eb0e4e1cb0f55af569eae7e0c8b79f475485b0124c5e85d720793c311385ee968fbd32992ced841877c1a'
